cceded5323eb021a18a8fa8cb950f045796173c9
airada <airada.seek@mail.kmutt.ac>
2023-03-13 17:11:22 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿class Pogram{
+    static void Main(string[] args){
+
+        Console.Write("Please input Day of week");
+        string Day = Console.ReadLine();
+       
+        Console.Write("Please input Time of week");
+        string Time = Console.ReadLine();
+
+        Console.Write("Please input Menu");
+        string Menu = Console.ReadLine();
+
+        int Monday =1;
+        int Tuesday =2;
+        int Wednesday =3;
+        int Thursday =4;
+        int Friday =5;
+        int Saturday =6;
+        int sunday =7;
+
+        int time8am = 8
+        int time9am = 9
+        int time10am = 10
+        int time11am = 11
+        int time12am = 12
+        int time13am = 13
+        int time14am = 14
+        int time15am = 15
+        int time16am = 16
+        int time17am = 17
+        int time18am = 18
+
+
+
+
+        
+       
+       int num Breakfast Set <=5
+       int num Weekend Set <=2
+       int num coffee  <=3
+
+        
+        case "Manu" :
+        if(Menu != Breakfast && Weekenk Set && Coffee){
+                Console.WriteLine("Please enter a valid")
+            }
+
+        switch(Breakfast Set) {
+            case "Breakfast Set" :
+            if Breakfast Set > 5
+            Console.Clear();
+            Console.WriteLine("Sorry your order is out of stock");
+            break;
+            case "Weekend Set" : 
+            if Weekend Set Sef > 2
+            Console.Clear();
+            Console.WriteLine("Sorry your order is out of stock");
+            break;
+             case "Coffee" :
+            if Weekend Set Sef > 3 
+            Console.Clear();
+            Console.WriteLine("Sorry your order is out of stock");
+            break;
+           
+            }
+
+          
+
+    
+
+    }
+    }
+
+       
+
+
+
+        
+
+
+
+
+
